2bcdf89efddb234828362bf5a2d893994ac661b4
unknown <oyeleyeoyedayo@gmail.com>
2021-12-05 16:15:46 +0000

    Import built-in react css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
 import './App.css';
+
+import config from './chatbot/config';
+import ActionProvider from './chatbot/ActionProvider';
+import MessageParser from './chatbot/MessageParser';
 
 function App() {
   return (
     <div className="App">
-      <div style={{ maxwidth: "300px" }}>
-        <Chatbot />
+      <div className="App-header" style={{ maxwidth: "300px" }}>
+        <Chatbot 
+          config={config} 
+          actionProvider={ActionProvider} 
+          messageParser={MessageParser} 
+        />
       </div>
     </div>
   );
