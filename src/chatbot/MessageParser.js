0df9ae31b3038291d7f518aee28afc4a47ef552d
unknown <oyeleyeoyedayo@gmail.com>
2021-12-05 16:51:47 +0000

    update

--- src/chatbot/MessageParser.js
+++ src/chatbot/MessageParser.js
@@ -5,7 +5,16 @@ class MessageParser {
     }
   
     parse(message) {
-      console.log(message)
+      console.log(message);
+      const lowercase = message.toLowerCase();
+
+      if (lowercase.includes("hello")) {
+          this.actionProvider.greet();
+      }
+
+      if (lowercase.includes("javascript") || lowercase.includes("js")) {
+          this.actionProvider.handleJavascriptQuiz();
+      }
     }
   }
   
