41849cd801c523a4a70c4819ec7648020565c247
unknown <oyeleyeoyedayo@gmail.com>
2021-12-05 15:56:17 +0000

    Import Options and Quiz components

--- src/chatbot/config.js
+++ src/chatbot/config.js
@@ -1,7 +1,26 @@
+import React from "react";
 import { createChatBotMessage } from "react-chatbot-kit";
+import Options from "../components/Options/Options";
+import Quiz from "../components/Quiz/Quiz";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)]
+  initialMessages: [createChatBotMessage(`Hello. What would you like to learn?`, {
+      widget: "options",
+  }),
+],
+
+  widgets: [
+      {
+        widgetName: "options",
+        widgetFunc: (props) => <Options {...props} />,
+        mapStateToProps: ["gist"],
+      },
+      {
+        widgetName: "quiz",
+        widgetFunc: (props) => <Quiz {...props} />,
+        mapStateToProps: ["gist"],
+      },
+  ],
 }
 
 export default config
