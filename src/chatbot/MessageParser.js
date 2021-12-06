81d6b456e7a323af05a3b885a272948ab87c7ed5
unknown <oyeleyeoyedayo@gmail.com>
2021-12-06 13:06:59 +0000

    Add more countries as conditional statement for other countries

--- src/chatbot/MessageParser.js
+++ src/chatbot/MessageParser.js
@@ -8,12 +8,28 @@ class MessageParser {
       console.log(message);
       const lowercase = message.toLowerCase();
 
-      if (lowercase.includes("hello")) {
+      if (lowercase.includes("hello") || lowercase.includes("hi") || lowercase.includes("bro")) {
           this.actionProvider.greet();
       }
 
-      if (lowercase.includes("javascript") || lowercase.includes("js")) {
-          this.actionProvider.handleJavascriptQuiz();
+      if (lowercase.includes("finland") || lowercase.includes("finnish")) {
+          this.actionProvider.handleFinlandQuiz();
+      }
+
+      if (lowercase.includes("norway") || lowercase.includes("norway")) {
+        this.actionProvider.handleNorwayQuiz();
+      }
+
+      if  (
+            lowercase.includes("germany") || lowercase.includes("italy") || lowercase.includes("canada") ||
+            lowercase.includes("australia") || lowercase.includes("sweden") || lowercase.includes("portugal") || 
+            lowercase.includes("slovekia") || lowercase.includes("slovenia") || lowercase.includes("belgium") ||
+            lowercase.includes("hungary") || lowercase.includes("france") || lowercase.includes("spain") ||
+            lowercase.includes("czech republic") || lowercase.includes("czech") || lowercase.includes("austria") ||
+            lowercase.includes("poland") || lowercase.includes("romania") || lowercase.includes("uk") || lowercase.includes("united kingdom")
+          ) 
+        {
+        this.actionProvider.handleOtherCountriesQuiz();
       }
     }
   }
